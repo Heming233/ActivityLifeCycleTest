6ec792fbbf8f1c9ef1b3cb7430644d8df2554a61
Heming233 <heming7159@163.com>
2024-02-04 17:08:37 +0800

    activity_main.xml Activity生命周期.png MainActivity.kt

--- app/src/main/java/com/example/activiytylifecycletest/MainActivity.kt
+++ app/src/main/java/com/example/activiytylifecycletest/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.activiytylifecycletest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Toast
 import com.example.activiytylifecycletest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,19 +18,30 @@ class MainActivity : AppCompatActivity() {
         Log.d(tag, "onCreate")
         setContentView(R.layout.activity_main)
 
+        //恢复Activity被销毁前的输入值
+        if(savedInstanceState != null){
+            val tempData = savedInstanceState.getString("data_key")
+            Log.d(tag, "tempData is $tempData")
+        }
+
         //ViewBinding
         //build.gradle修改后记得同步项目
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //设置normalActivity按钮
         binding.startNormalActivity.setOnClickListener({
+            Toast.makeText(this, "You clicked 'Start Normal'", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, NormalActivity::class.java)
             startActivity(intent)
+            Log.d(tag, "NormalActivity has been started!")
         })
 
         //设置dialogActivity按钮
         binding.startDialogActivity.setOnClickListener({
             val intent = Intent(this, DialogActivity::class.java)
+            startActivity(intent)
+            Log.d(tag, "DialogActivity has been started!")
         })
     }
 
@@ -45,6 +58,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //在系统准备启动或恢复另一个Activity时调用，释放一些消耗CPU的资源、保存一些关键数据
+    //Activity在onResume和onPause之间经历的是一个前台生存期
     override fun onPause() {
         super.onPause()
         Log.d(tag, "onPause")
@@ -52,11 +66,13 @@ class MainActivity : AppCompatActivity() {
 
     //在Activity完全不可见时调用。如果新Activity是一个对话框，那么onStop不会执行
     //执行的是onPause
+    //Activity在onStart和onStop之间经历的是一个可见生存期
     override fun onStop() {
         super.onStop()
         Log.d(tag, "onStop")
     }
 
+    //Activity在onCreate和onDestroy之间经历的就是一个完整的生存期
     override fun onDestroy() {
         super.onDestroy()
         Log.d(tag, "onDestroy")
@@ -66,5 +82,12 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         Log.d(tag, "onRestart")
+    }
+
+    //重写保存实例状态函数
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        val tempData = "Something you just typed"
+        outState.putString("data_key", tempData)
     }
 }
